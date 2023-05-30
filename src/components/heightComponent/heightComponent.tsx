919fa888fcad51f7ad4c1f53fec9a477ee6ce4ca
Jaque <j.costa@dino.com.br>
2023-05-30 18:39:12 -0300

    remove components

--- src/components/heightComponent/heightComponent.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "./styles";
-
-export function HeightComponent({ children, style }) {
-  return (
-    <>
-      <Container style={style}>{children}</Container>
-    </>
-  );
-}
