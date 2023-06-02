33f7b94443d28e55a2dabb878f1f7e242207d65f
Jaque <j.costa@dino.com.br>
2023-06-02 18:06:03 -0300

    remove line

--- src/components/line/line.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "./styles";
-
-export type ILine = {
-  direction: "horizontal" | "vertical";
-};
-
-export default function Line({ direction }: ILine) {
-  return <Container direction={direction} />;
-}
