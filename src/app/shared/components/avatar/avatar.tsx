5125f7ee375c68d609f7ec52fa9ee8984d03bd1c
Jaque <email@example.com>
2023-10-25 19:08:15 -0300

    avatar

---
+++ src/app/shared/components/avatar/avatar.tsx
@@ -0,0 +1,85 @@
+import { faUser } from "@fortawesome/pro-regular-svg-icons";
+import { faCamera } from "@fortawesome/pro-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  Container,
+  HoverChangeImage,
+  Image,
+  Initials,
+  LabelActive,
+} from "./styles";
+
+export type IAvatar = {
+  children?: React.ReactNode;
+  size: "small" | "medium" | "large" | "xlarge";
+  active?: "on" | "off";
+  image?: string;
+  name?: string;
+  id?: string;
+  label?: string;
+  onClick?: () => void;
+  handleChangeFile?: (e) => void;
+  edit?: true;
+};
+
+export default function Avatar({
+  size,
+  image,
+  active,
+  label,
+  onClick,
+  handleChangeFile,
+  edit,
+}: IAvatar) {
+  const randomColor = () => {
+    let hex = Math.floor(Math.random() * 0xffffff);
+    let color = "#" + hex.toString(16);
+    return color;
+  };
+
+  const handleImage = () => {
+    if (image) {
+      return <Image src={image} />;
+    } else if (label && label.length <= 2) {
+      return (
+        <Initials
+          style={{
+            backgroundColor: randomColor(),
+            color: "#FFF",
+          }}
+        >
+          {label}
+        </Initials>
+      );
+    } else {
+      return <FontAwesomeIcon icon={faUser} />;
+    }
+  };
+
+  const handleActive = () => {
+    if (active) {
+      return <LabelActive active={active} />;
+    }
+  };
+
+  const handleChangeImage = () => {
+    if (edit) {
+      return (
+        <HoverChangeImage>
+          <FontAwesomeIcon icon={faCamera} />
+          {/* <input hidden id="uploadImage" name="file" type="file" accept=".jpg, .png, .jpeg, .webp" onChange={(e) => a(e)}/> */}
+        </HoverChangeImage>
+      );
+    }
+  };
+
+  return (
+    <>
+      <Container size={size} onClick={onClick} edit={edit}>
+        {handleImage()}
+        {handleActive()}
+        {handleChangeImage()}
+      </Container>
+    </>
+  );
+}
