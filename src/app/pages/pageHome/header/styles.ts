a34d545e72cfc27d0d587b61bf49db251020cd77
Jaque <email@example.com>
2023-11-08 18:47:02 -0300

    initial header

---
+++ src/app/pages/pageHome/header/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  top: 0;
+  background-color: #222;
+  padding: 12px 19px 12px 24px;
+  width: 100%;
+  height: 56px;
+  gap: 30px;
+  z-index: 1;
+`;
