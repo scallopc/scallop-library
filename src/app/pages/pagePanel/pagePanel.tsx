759971a40b1857a428e70528819aee7232c6330a
Jaque <email@example.com>
2023-12-06 20:24:26 -0300

    Panel is a reusable

--- src/app/pages/pagePanel/pagePanel.tsx
+++ src/app/pages/pagePanel/pagePanel.tsx
@@ -34,13 +34,7 @@ export function PagePanel() {
                     <H5>Documentation</H5>
                     <DocumentationContainer>
                          <p>
-                              ToggleSwitch is a reusable component that displays
-                              a switch visually represented by a slider. It
-                              allows users to toggle between two states, usually
-                              represented by "on" and "off". The component can
-                              be used in web applications to provide an
-                              intuitive way to enable or disable a
-                              functionality, option or setting.
+                              Panel is a reusable component ..
                          </p>
 
                          <Content>
