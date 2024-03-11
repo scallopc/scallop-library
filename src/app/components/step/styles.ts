3502704a366440125bfb06da6eee47b78e4bbd7d
Jaque Costa <jaqueline.costa@eleevait.com.br>
2024-03-11 18:54:56 -0300

    styled stepper

---
+++ src/app/components/step/styles.ts
@@ -0,0 +1,9 @@
+import styled, { css } from "styled-components";
+
+export const Stepper = styled.div`
+     position: relative;
+     display: flex;
+     justify-content: space-between;
+     align-items: center;
+     margin-bottom: 20px;
+`;
